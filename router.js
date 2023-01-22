335b49ddfff8cb920cb39de21370993b30111fc0
Malwina <yellowindmills@gmail.com>
2023-01-22 16:15:44 +0100

    first commit

---
+++ router.js
@@ -0,0 +1,27 @@
+let pageUrls = {
+    about: '/index.html?about'
+}
+function onStartUp(params) {
+    PopStateHandler();
+}
+onStartUp();
+document.querySelector('#about-link').addEventListener('click', (event) => {
+    let stateObj = {
+        page: 'about'
+    }
+    document.title = 'About'
+    history.pushState(stateObj, 'about', '?about');
+RenderAboutPage();
+});
+
+function RenderAboutPage(params) {
+    document.getElementById('about');
+}
+
+function PopStateHandler(event) {
+    loc = window.location.href.toString().split(window.location.host)[1];
+    if (loc === pageUrls.about){
+        RenderAboutPage();
+    }
+}
+window.onpopstate = PopStateHandler;
